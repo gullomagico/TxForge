469c40ee8c6d24bebab94a0d121b8bfe96fcc86d
gullomagico <33353590+gullomagico@users.noreply.github.com>
2024-04-02 00:46:48 +0200

    Add AddContractModal and Card components

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let title: string;
+	export let description: string;
+</script>
+
+<div>
+	<div>
+		<img src="https://via.placeholder.com/150" alt="Placeholder" />
+	</div>
+	<div>
+		<h2>{title}</h2>
+		<p>{description}</p>
+	</div>
+	<div>
+		<button>Click me</button>
+	</div>
+</div>
+
+<style>
+</style>
